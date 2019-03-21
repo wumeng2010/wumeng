0540dac2aedae48c18afa3084f3a0c8a1661c75e
wumeng <1511095652@qq.com>
2019-03-21 20:45:59 +0800

    添加到本地仓库

---
+++ Git/src/com/meng/test.java
@@ -0,0 +1,9 @@
+package com.meng;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("成功");
+        System.out.println("你好啊");
+    }
+}
